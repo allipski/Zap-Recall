50e650ed659f86f8242d2ed127834cf1bb77c0e2
Alice Lipski <alipski@pop-os.localdomain>
2022-07-22 17:50:32 -0300

    organização de pastas e primeiros passos

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.png'
+
+export default function App() {
+    return (
+        <div className="master">
+            <img src={logo} />
+            <button className="startbutton"></button>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import "./components/reset.css";
+import "./components/styles.css";
+import App from './components/App'
+
+ReactDOM.render(<App />, document.querySelector(".root"))
